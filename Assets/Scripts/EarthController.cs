b71597fedf0a00ee953c59b447d884c9feb2a17e
Warwick Stone <warwick_stone@fastmail.com>
2023-07-20 19:27:10 +1000

    earth controller uses earth rotation calc. pos still not quite right

--- Assets/Scripts/EarthController.cs
+++ Assets/Scripts/EarthController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 public class EarthController : MonoBehaviour
 {
     private TimeController _timeController;
-    private Quaternion _initialRotation;
-    private EarthCalcs _earthCalcs = new();
+    private readonly EarthCalcs _earthCalcs = new();
 
     void Start()
     {
         _timeController = GameObject.Find("TimeControl").GetComponent<TimeController>();
-        _initialRotation = transform.rotation;
 
         _earthCalcs.SetReference(
             new Vector3(0, 0, 60),               // z is north in Unity coords
@@ -21,26 +19,6 @@ public class EarthController : MonoBehaviour
     void Update()
     {
         transform.position = _earthCalcs.Position(_timeController.CurrentUtcTime());
-        Rotate();
-    }
-
-    private void Rotate()
-    {
-        // 'zero' rotation in Unity is Sun over ~-90 longitude
-        // rotate by -90 sets Earth to 12am UTC
-        var offset = -90;
-
-        var lastDecSolstice = _timeController.LastDecSolstice();
-        var rotationAtLastSolstice = (float)lastDecSolstice.TimeOfDay.TotalDays * 360;
-
-        const int secondsPerSiderealDay = 23 * 3600 + 56 * 60;
-        var siderealDays = _timeController.SecondsSinceDecSolstice() / secondsPerSiderealDay;
-        var rotatedSinceSolstice = siderealDays * 360;
-
-        var currentRotation = rotationAtLastSolstice + rotatedSinceSolstice + offset;
-
-        transform.rotation = _initialRotation * Quaternion.AngleAxis(
-            (float)currentRotation,
-            Vector3.up);
+        transform.rotation = _earthCalcs.Rotation(_timeController.CurrentUtcTime());
     }
 }
